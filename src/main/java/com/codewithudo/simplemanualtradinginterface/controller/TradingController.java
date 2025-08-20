7b825f1f072dcfa895a534120f2a57ccd9c80390
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-08-20 01:49:05 +0100

    feat: Add TradingController for placing orders
    Creates a REST controller with a POST endpoint at '/api/v1/orders'.
    
    - Uses @PostMapping to handle the creation of new trade orders.
    - Uses @RequestBody to map the incoming JSON to a CreateOrderRequest DTO.
    - Delegates the request to the TradingService.

--- src/main/java/com/codewithudo/simplemanualtradinginterface/controller/TradingController.java
+++ src/main/java/com/codewithudo/simplemanualtradinginterface/controller/TradingController.java
@@ -1,4 +1,25 @@
 package com.codewithudo.simplemanualtradinginterface.controller;
 
+import com.codewithudo.simplemanualtradinginterface.dto.CreateOrderRequest;
+import com.codewithudo.simplemanualtradinginterface.dto.OrderDetails;
+import com.codewithudo.simplemanualtradinginterface.service.TradingService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TradingController {
+
+    private final TradingService tradingService;
+
+    public TradingController(TradingService tradingService) {
+        this.tradingService = tradingService;
+    }
+
+    @PostMapping("/orders")
+    public OrderDetails placeOrder(@RequestBody CreateOrderRequest request) {
+        return tradingService.placeOrder(request);
+    }
 }
